46ebf77064d8ea31ad3ed94e344374e0d39d249f
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-03-27 07:57:11 +0200

    Initial project files

---
+++ Finglish/src/main/java/finglish/ui/FinglishUi.java
@@ -0,0 +1,18 @@
+
+package finglish.ui;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FinglishUi {
+    
+ 
+    public void start(Stage primaryStage) {
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("terve");
+    }
+
+}
